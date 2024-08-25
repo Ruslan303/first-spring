88d87e0954dc0d4cae62a8dfad65385067e1b8b5
Ruslan Huseynov <rh4685225@gmail.com>
2024-08-25 14:27:59 +0400

    first spring

---
+++ src/main/java/az/firspp/FirsPpApplication.java
@@ -0,0 +1,13 @@
+package az.firspp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirsPpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FirsPpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/az/firspp/FirsPpApplicationTests.java
@@ -0,0 +1,13 @@
+package az.firspp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirsPpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
